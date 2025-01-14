ded3455a2607eb308051925ff0a67fd0dfa18bca
Febby Salma Nadia <128360391+febbysalmaa@users.noreply.github.com>
2025-01-14 21:06:37 +0700

    Updatekedua c.py

--- games/c.py
+++ games/c.py
@@ -1,12 +1,13 @@
-#########
+#
 def c():
         n = int(input("Masukan Nilai Awalnya : "))
         if True:
             nilai = [int(digit) for digit in str(n)]
+            pangkat= len(str(n))
             hasil = []
             nilai_awal = 1
             for i in nilai:
-                nilai_awal = nilai_awal * (i**3)
+                nilai_awal = nilai_awal * (i**pangkat)
                 hasil.append(nilai_awal)
                 nilai_awal = 1
 
